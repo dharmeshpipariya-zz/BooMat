bbefc27853be129151f76b3a15695b30694fed1b
dharmeshpipariya <pipariyadharmesh@gmail.com>
2016-01-28 18:35:49 +0530

    update Tooltip style

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,40 +1,39 @@
 /*!
  * BooMat's Gruntfile
  * https://github.com/DharmeshPipariya/BooMat
- * Copyright 2015 BooMat.
+ * Copyright 2016 BooMat.
  */
 
 module.exports = function (grunt) {
     grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
-        //watch: {
-        //    sass: {
-        //        files: ['sass/**/*.{scss,sass}', 'sass/_partials/**/*.{scss,sass}'],
-        //        tasks: ['sass:dist']
-        //    },
-        //    livereload: {
-        //        files: ['*.html', '*.php', 'js/**/*.{js,json}', 'css/*.css', 'img/**/*.{png,jpg,jpeg,gif,webp,svg}'],
-        //        options: {
-        //            livereload: true
-        //        }
-        //    }
-        //},
         sass: {
-            options: {
-                sourceMap: true,
-                outputStyle: 'compressed'
-            },
             dist: {
-                files: {
-                    'dist/css/boomat.css': 'sass/_boomat.scss'
-                }
+                files: [{
+                    src: 'scss/*.scss',
+                    dest: 'dist/css',
+                    expand: true,
+                    ext: '.css'
+                }]
+            }
+        },
+        autoprefixer: {
+            dist: {
+                files: [{
+                    expand: true,
+                    cwd: '.dist/css',
+                    src: '{,*/}*.css',
+                    dest: 'dist/css'
+                }]
+            }
+        },
+        watch: {
+            styles: {
+                files: ['scss/*.scss'],
+                tasks: ['sass:dist', 'autoprefixer:dist']
             }
         }
     });
-    //grunt.registerTask('default', ['sass:dist', 'watch']);
-    grunt.registerTask('default', ['sass:dist']);
-    grunt.loadNpmTasks('grunt-sass');
-    //grunt.loadNpmTasks('grunt-contrib-watch');
-    //require('load-grunt-tasks')(grunt);
-    //grunt.registerTask('default', ['sass']);
+    grunt.registerTask('default', ['styles', 'watch']);
 };
+
+
