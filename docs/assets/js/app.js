34a5174f1e910917624f5b1c2c1c12e2d4adc061
Dharmesh <pipariyadharmesh@gmail.com>
2016-02-03 23:43:58 +0530

    update style and document structure.

--- docs/assets/js/app.js
+++ docs/assets/js/app.js
@@ -1,39 +1,21 @@
-// create the module and name it boomatApp
 var boomatApp = angular.module('boomatApp', ['ngRoute']);
 
-// configure our routes
 boomatApp.config(function ($routeProvider) {
     $routeProvider
-
-        // route for the home page
         .when('/', {
-            templateUrl: 'pages/home.html',
+            templateUrl: 'docs/index.html',
             controller: 'indexController'
         })
-
-        // route for the about page
-        .when('/about', {
-            templateUrl: 'pages/about.html',
-            controller: 'aboutController'
+        .when('/components', {
+            templateUrl: 'docs/components/index.html',
+            controller: 'componentsController'
         })
-
-        // route for the contact page
-        .when('/contact', {
-            templateUrl: 'pages/contact.html',
-            controller: 'contactController'
-        });
+        .when('/components/tooltip', {
+            templateUrl: 'docs/components/tooltip.html',
+            controller: 'componentsController'
+        })
 });
 
-// create the controller and inject Angular's $scope
-boomatApp.controller('indexController', function ($scope) {
-    // create a message to display in our view
-    $scope.message = 'Everyone come and see how good I look!';
-});
+boomatApp.controller('indexController', function ($scope) { });
 
-boomatApp.controller('aboutController', function ($scope) {
-    $scope.message = 'Look! I am an about page.';
-});
-
-boomatApp.controller('contactController', function ($scope) {
-    $scope.message = 'Contact us! JK. This is just a demo.';
-});
+boomatApp.controller('componentsController', function ($scope) { });
